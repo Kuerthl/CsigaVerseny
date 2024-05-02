9307a5bbf106cfad5c7b6fe1deed250a46fe43f1
kthla <kthla@DESKTOP-AK6EDQJ.localdomain>
2024-05-02 14:21:12 +0200

    Main implemented

--- src/csigaverseny/CsigaVerseny.java
+++ src/csigaverseny/CsigaVerseny.java
@@ -3,18 +3,51 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package csigaverseny;
-
+import java.util.Scanner;
 /**
  *
  * @author kthla
  */
 public class CsigaVerseny {
 
+    static final int ROUNDS = 5;
+    static final int PAUSE_SEC = 3;
+    static Scanner scan = new Scanner(System.in);
+    static IGame nGame = new Game();
     
-    
-    public static void main(String[] args) 
+    public static void main(String[] args) throws InterruptedException
     {
-        System.out.println("Lefutott");
+        WriteOut();
+        System.out.println("\nMelyik csigára szeretne fogadni?");
+        System.out.println("Csiga száma: ");
+        int index = scan.nextInt();
+        
+        for(int i=0; i<ROUNDS; i++)
+        {
+            nGame.GameOn();
+            System.out.println("\nAz " + (i+1) + ". kör állása a következő: ");
+            WriteRounds();
+            
+            Thread.sleep(PAUSE_SEC*1000);
+        }
+        nGame.getWinnerSnail();
+ 
+    }
+    
+    static void WriteOut()
+    {
+        for(int i=0; i<nGame.getSnails().length; i++)
+        {
+            System.out.println("\n" + (i+1) + ". csiga: " + nGame.getSnails()[i]);
+        }
+    }
+    
+    static void WriteRounds()
+    {
+        for(int i=0; i<nGame.getSnails().length; i++)
+        {
+            System.out.println("\nAz " + (i+1) + ". helyezett: " + nGame.getSnails()[i]);
+        }
     }
     
 }
